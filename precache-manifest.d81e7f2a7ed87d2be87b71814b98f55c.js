6b24b0041ceff48b5cb70da5821d1eb6cd16af90
mattShaverWebCreator <matt@boostthegram.com>
2019-12-06 22:03:33 -0500

    Updates

---
+++ precache-manifest.d81e7f2a7ed87d2be87b71814b98f55c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "087a823d692c112d12abc6249da24c62",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "70e8a5cf36618ca2c487",
+    "url": "/monsters-rolodex/static/css/main.fc7e41f9.chunk.css"
+  },
+  {
+    "revision": "5ffb6fab3c6fb8afc674",
+    "url": "/monsters-rolodex/static/js/2.f6373902.chunk.js"
+  },
+  {
+    "revision": "70e8a5cf36618ca2c487",
+    "url": "/monsters-rolodex/static/js/main.ee8b4500.chunk.js"
+  },
+  {
+    "revision": "03497ef441aebc7cc47b",
+    "url": "/monsters-rolodex/static/js/runtime-main.666613d3.js"
+  }
+]);
